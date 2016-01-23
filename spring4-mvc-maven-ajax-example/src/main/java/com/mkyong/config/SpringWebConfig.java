65b57a0c71c6ccf7f32259a509441bc96437deb4
yewz <weiwenz@yahoo.com>
2016-01-23 18:33:11 -0500

    Update SpringWebConfig.java
    changed 6:33

--- spring4-mvc-maven-ajax-example/src/main/java/com/mkyong/config/SpringWebConfig.java
+++ spring4-mvc-maven-ajax-example/src/main/java/com/mkyong/config/SpringWebConfig.java
@@ -16,7 +16,7 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
  
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/"); //test 6:29
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/"); //test 6:32
 	}
 	
 	@Bean
