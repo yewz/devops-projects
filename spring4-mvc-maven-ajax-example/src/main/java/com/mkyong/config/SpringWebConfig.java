22f6bd65c2e9ac7a1e38ed8aabfee7c14372ab11
yewz <weiwenz@yahoo.com>
2016-01-23 23:56:46 -0500

    add file

--- spring4-mvc-maven-ajax-example/src/main/java/com/mkyong/config/SpringWebConfig.java
+++ spring4-mvc-maven-ajax-example/src/main/java/com/mkyong/config/SpringWebConfig.java
@@ -16,7 +16,7 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
  
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/"); 
 	}
 	
 	@Bean
