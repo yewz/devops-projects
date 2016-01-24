e6f2716c73d386cb046cfa406830d735b2a8d56b
yewz <weiwenz@yahoo.com>
2016-01-23 20:04:52 -0500

    test add in git bash

--- spring4-mvc-maven-ajax-example/src/main/java/com/mkyong/config/SpringWebConfig.java
+++ spring4-mvc-maven-ajax-example/src/main/java/com/mkyong/config/SpringWebConfig.java
@@ -16,7 +16,7 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
  
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");  //test 7:32
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");  //test 7:34
 	}
 	
 	@Bean
